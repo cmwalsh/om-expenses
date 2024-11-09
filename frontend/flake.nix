1e5ebc896a1b7d0c2aed83e29dc6b974331ad50f
Chris Dell <cjdell@gmail.com>
2024-11-09 20:08:27 +0000

    Fix flake

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
-    { nixpkgs, prisma-utils, ... }:
+    { nixpkgs, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
