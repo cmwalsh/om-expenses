7139612440866e1cda82092b53a40cec337db809
Chris Dell <cjdell@gmail.com>
2024-12-09 12:40:03 +0000

    Use fixed tmp build dir

--- flake.nix
+++ flake.nix
@@ -25,12 +25,12 @@
         let
           hashes = {
             aarch64-darwin = "sha256-CZdRMTO38wKv8CBJo1UjCcrVxymSDnITG5wG50Gaib0=";
-            x86_64-linux = "sha256-X5mH7CFjYfOtn632ZFsv8bfCkI/MCWCMJI9Pi5c/8zc=";
+            x86_64-linux = "sha256-dpb83DlBAawnGvKV8HukIk3p5JPk3gJLv7z75d5mTvA=";
           };
         in
         pkgs.stdenv.mkDerivation {
           pname = "om-expenses";
-          version = "0.0.10";
+          version = "0.0.11";
 
           src = ./.;
 
@@ -48,9 +48,9 @@
 
             export HOME="$(mktemp -d)"
 
-            mkdir build-inner
-            mv !(build-inner) build-inner/
-            cd build-inner
+            mkdir /tmp/build-inner
+            mv * /tmp/build-inner/
+            cd /tmp/build-inner
 
             ${pkgs.deno}/bin/deno cache backend/src/index.ts
             ${pkgs.deno}/bin/deno cache frontend/bundle.ts
@@ -66,6 +66,8 @@
             ${pkgs.deno}/bin/deno compile --cached-only --no-code-cache -o $out/bin/om-expenses-backend   backend/src/index.ts
             echo "Compiling frontend..."
             ${pkgs.deno}/bin/deno compile --cached-only --no-code-cache -o $out/bin/om-expenses-frontend  frontend/server.ts
+
+            rm -rf /tmp/build-inner
           '';
         };
     });
